9945efed57e8491810aa09bf8a2b1e0578af7f1c
baehj <101856005+baehj@users.noreply.github.com>
2022-11-15 23:57:55 +0900

    221115 코드업100제 ~10번
    221115 코드업100제 ~10번

--- Test/test1.py
+++ Test/test1.py
@@ -1,3 +1,24 @@
 # 문제1
-print("Hello");
-# 문제1
+print("Hello")
+# 문제2
+print("Hello World")
+# 문제3
+print("Hello")
+print("World")
+# 문제4
+print("'Hello'")
+# 문제5
+print('"Hello World"')
+# 문제6
+print('"' + '!@#$%^&*()' + "'")
+print("\"!@#$%^&*()'")
+# 문제7 \와 '를 나오게 하려면 앞에 \를 하나씩 더 붙여준다
+print('"C:\\Download\\\'hello\'.py"')
+# 문제8
+print('print("Hello\\nWorld")')
+# 문제9
+print(input())
+# 문제10
+n = input()
+n = int(n)
+print(n)
